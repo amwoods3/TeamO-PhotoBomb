f3189da0f18cbcded1770e7a14758da967f911e0
amwoods3 <amwoods3@cougars.ccis.edu>
2014-12-12 04:10:45 -0600

    Add HTML functions.

---
+++ html.py
@@ -0,0 +1,12 @@
+def beginHTML():
+    return """Content-Type: text/html;charset=utf-8
+
+    <h3 align="Center"> <font size="7" color="blue">Photo Bomb</font> </h3>
+    <html>
+    """
+
+def endHTML():
+    return """
+    </body>
+    </html>
+    """
