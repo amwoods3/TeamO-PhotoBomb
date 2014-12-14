aa561bc8804e2e977ada18a3d56e2dbbb894b6a7
root <root@localhost.localdomain>
2014-12-13 21:38:04 -0600

    Added color to background

--- TeamOhtml.py
+++ TeamOhtml.py
@@ -1,9 +1,9 @@
 def beginHTML():
     return """Content-Type: text/html;charset=utf-8
 
-    <h3 align="Center"> <font size="7" color="blue">Photo Bomb</font> </h3>
+    <h3 align="Center"> <font size="7" color="blue">Photo Bomb!</font> </h3>
     <html>
-    <body>
+    <body style="background-color:Beige">
     """
 
 def endHTML():
